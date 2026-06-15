2cfab85d9bc4b2f410c8b45a1a399cf4aec37f1d
eunsu-source <0526eunsu@snu.ac.kr>
2026-06-15 12:35:05 +0900

    Initial commit

---
+++ images.js
@@ -0,0 +1,16 @@
+// 이미지 관리 파일
+// 새 이미지를 추가/교체하려면:
+// 1. images 폴더에 이미지 파일을 넣고
+// 2. 아래 목록에 "키": "images/파일명" 형식으로 등록하세요.
+// 키 이름은 index.html의 ARTICLES 데이터에서 사용하는 이미지 id와 일치해야 합니다.
+const IMAGES={
+  "choi_1": "images/choi_1.jpg",
+  "choi_2": "images/choi_2.jpg",
+  "choi_3": "images/choi_3.jpg",
+  "choi_4": "images/choi_4.jpg",
+  "choi_5": "images/choi_5.jpg",
+  "lee_1": "images/lee_1.jpg",
+  "lee_2": "images/lee_2.jpg",
+  "lee_3": "images/lee_3.jpg",
+  "lee_4": "images/lee_4.jpg"
+};
